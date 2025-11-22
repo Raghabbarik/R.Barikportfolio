2dfb30e609da426eea931833ed7dc5e3a29b7845
Raghab Barik Raghab Barik <rraghabbarik@gmail.com>
2025-11-22 17:09:00 +0000

    show this error

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -37,6 +38,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'instasize.com',
         port: '',
         pathname: '/**',
       }
