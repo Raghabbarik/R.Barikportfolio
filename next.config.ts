dd891f2c2761f41954a7a2fbced18ae76bbbb887
Raghab Barik Raghab Barik <rraghabbarik@gmail.com>
2025-11-22 16:46:21 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'old-gray-uyua0uwjot.edgeone.app',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   experimental: {
