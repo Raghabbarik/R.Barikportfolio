e4e984868791fad08fc1b43a4dc24fc4beaaef5a
Raghab Barik Raghab Barik <rraghabbarik@gmail.com>
2025-11-23 18:04:39 +0000

    i can push my webside in github  and publish by website in github but we

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
 
 import type {NextConfig} from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY!.replace(/.*?\//, '')
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -10,6 +25,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+   unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -48,7 +64,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'images.pexels.com',
         port: '',
         pathname: '/**',
