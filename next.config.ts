55bd496095c613877ca4635ca32a894d35d5780c
Raghab Barik Raghab Barik <rraghabbarik@gmail.com>
2025-11-23 18:25:02 +0000

    same this  problem are show  what is this explain and solve this

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,12 @@
 
 import type {NextConfig} from 'next';
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY!.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+const repo = 'R.Barikportfolio'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
 
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
   assetPrefix: assetPrefix,
   basePath: basePath,
