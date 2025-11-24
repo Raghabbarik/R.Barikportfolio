321f21a29d51979e2a9f315ada079ff541a62081
Raghab Barik Raghab Barik <rraghabbarik@gmail.com>
2025-11-24 20:28:02 +0000

    remove the ballpit background

--- src/components/global-background.tsx
+++ src/components/global-background.tsx
@@ -1,15 +1,7 @@
-
 'use client';
 
-import dynamic from "next/dynamic";
-import { Suspense } from "react";
 import { usePathname } from "next/navigation";
 import { useTheme } from "next-themes";
-
-const Ballpit = dynamic(() => import('@/components/ballpit-background'), {
-  ssr: false,
-});
-
 
 export default function GlobalBackground() {
     const pathname = usePathname();
@@ -17,14 +9,7 @@ export default function GlobalBackground() {
 
     if(pathname.includes('admin')) {
       setTheme('dark');
-      return null;
     }
 
-    return (
-        <div className="fixed top-0 left-0 w-full h-full z-0">
-            <Suspense fallback={<div className="w-full h-full bg-background" />}>
-                <Ballpit />
-            </Suspense>
-        </div>
-    );
+    return null;
 }
