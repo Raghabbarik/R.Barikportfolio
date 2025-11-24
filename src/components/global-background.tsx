5348315c8be43a864b67830377376f8237efc467
Raghab Barik Raghab Barik <rraghabbarik@gmail.com>
2025-11-24 20:12:41 +0000

    add this background  style  in home page also all pages

--- src/components/global-background.tsx
+++ src/components/global-background.tsx
@@ -3,21 +3,27 @@
 
 import dynamic from "next/dynamic";
 import { Suspense } from "react";
+import { usePathname } from "next/navigation";
+import { useTheme } from "next-themes";
 
-const Threads = dynamic(() => import('@/components/threads-background'), {
+const Ballpit = dynamic(() => import('@/components/ballpit-background'), {
   ssr: false,
 });
 
+
 export default function GlobalBackground() {
+    const pathname = usePathname();
+    const {setTheme} = useTheme();
+
+    if(pathname.includes('admin')) {
+      setTheme('dark');
+      return null;
+    }
+
     return (
         <div className="fixed top-0 left-0 w-full h-full z-0">
             <Suspense fallback={<div className="w-full h-full bg-background" />}>
-            <Threads 
-                color={[0.4, 0.2, 0.8]} 
-                amplitude={0.5}
-                distance={0.1}
-                enableMouseInteraction={true} 
-            />
+                <Ballpit />
             </Suspense>
         </div>
     );
