0ebe6bfc2d520a4e434263128dced165e42bdeb6
Raghab Barik Raghab Barik <rraghabbarik@gmail.com>
2025-11-24 06:27:53 +0000

    alredy email and password are swoing

--- src/components/admin/login-form.tsx
+++ src/components/admin/login-form.tsx
@@ -42,8 +42,8 @@ export function LoginForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      email: "rraghabbarik@gmail.com",
-      password: "Raghab@2006",
+      email: "",
+      password: "",
     },
   });
 
