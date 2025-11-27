8472c0661b579b3957c0b30e9099527f4c1b8e3a
Raghab Barik Raghab Barik <rraghabbarik@gmail.com>
2025-11-27 17:49:17 +0000

    slightly move to upword

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -23,7 +23,7 @@ export function ThemeToggle() {
   }
 
   return (
-    <div className="fixed bottom-5 left-5 z-50">
+    <div className="fixed bottom-10 left-5 z-50">
         <Button variant="outline" size="icon" onClick={toggleTheme} className="rounded-full h-12 w-12 shadow-lg">
             <Sun className="h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
